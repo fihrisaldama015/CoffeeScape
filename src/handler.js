e6b195d0c3c63a84db0a7f7c094d517bf1aa80f9
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-11-26 19:39:49 +0700

    [FIX] unused comments

--- src/handler.js
+++ src/handler.js
@@ -1,44 +1,22 @@
 const { nanoid } = require("nanoid");
 const books = require("./books");
 const arabica = require("./arabica");
-// const admin = require("firebase-admin");
-// const serviceAccount = require("../serviceAccount.json"); // Replace with your service account key
 const { db } = require("./lib/firebase");
-const {
-  addDoc,
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-} = require("firebase/firestore");
 
 const getArabica = async (request, h) => {
   try {
-    // const querySnapshot = await getDocs(collection(db, "arabica"));
-    // querySnapshot.forEach((doc) => {
-    //   const temp = {
-    //     id: doc.id,
-    //     ingredients: doc.data().ingredients,
-    //     name: doc.data().name,
-    //     steps: doc.data().steps,
-    //   };
-    //   arabica.push(temp);
-    // });
     let arabica = [];
     await db
       .collection("arabica")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
           const temp = {
             id: doc.id,
             ingredients: doc.data().ingredients,
             name: doc.data().name,
             steps: doc.data().steps,
           };
-          // console.log(arabica);
-          console.log("aowkawok" + doc.id);
           arabica.push(temp);
         });
       });
@@ -63,14 +41,6 @@ const getArabica = async (request, h) => {
 };
 
 const addData = (request, h) => {
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(serviceAccount),
-  //   databaseURL: "https://credible-bay-405515.asia-southeast2.firebaseio.com/", // Replace with your database URL
-  // });
-
-  // Reference to your Firestore database
-  // const db = admin.firestore();
-  // Reference to the collection
   const recipesCollection = db.collection("arabica");
   // Add the Espresso recipe to Firebase
   try {
