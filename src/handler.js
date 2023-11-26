11caf2104900dcd204963845abcc82118c1887bd
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-11-26 19:36:07 +0700

    [ADD] get all arabica

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,8 @@
 const { nanoid } = require("nanoid");
 const books = require("./books");
 const arabica = require("./arabica");
-const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccount.json"); // Replace with your service account key
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../serviceAccount.json"); // Replace with your service account key
 const { db } = require("./lib/firebase");
 const {
   addDoc,
@@ -91,18 +91,36 @@ const addBook = (request, h) => {
 
 const getArabica = async (request, h) => {
   try {
+    // const querySnapshot = await getDocs(collection(db, "arabica"));
+    // querySnapshot.forEach((doc) => {
+    //   const temp = {
+    //     id: doc.id,
+    //     ingredients: doc.data().ingredients,
+    //     name: doc.data().name,
+    //     steps: doc.data().steps,
+    //   };
+    //   arabica.push(temp);
+    // });
     let arabica = [];
-    const querySnapshot = await getDocs(collection(db, "arabica"));
-    querySnapshot.forEach((doc) => {
-      const temp = {
-        id: doc.id,
-        ingredients: doc.data().ingredients,
-        name: doc.data().name,
-        steps: doc.data().steps,
-      };
-      arabica.push(temp);
-    });
+    await db
+      .collection("arabica")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          const temp = {
+            id: doc.id,
+            ingredients: doc.data().ingredients,
+            name: doc.data().name,
+            steps: doc.data().steps,
+          };
+          // console.log(arabica);
+          console.log("aowkawok" + doc.id);
+          arabica.push(temp);
+        });
+      });
 
+    console.log("arabica => ", arabica);
     const response = h.response({
       status: "success",
       message: "get Arabica successfully",
@@ -122,13 +140,13 @@ const getArabica = async (request, h) => {
 };
 
 const addData = (request, h) => {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://credible-bay-405515.asia-southeast2.firebaseio.com/", // Replace with your database URL
-  });
+  // admin.initializeApp({
+  //   credential: admin.credential.cert(serviceAccount),
+  //   databaseURL: "https://credible-bay-405515.asia-southeast2.firebaseio.com/", // Replace with your database URL
+  // });
 
   // Reference to your Firestore database
-  const db = admin.firestore();
+  // const db = admin.firestore();
   // Reference to the collection
   const recipesCollection = db.collection("arabica");
   // Add the Espresso recipe to Firebase
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,32 @@
-const { initializeApp } = require("firebase/app");
-const { getAuth } = require("firebase/auth");
-const { getFirestore } = require("firebase/firestore");
-const { GoogleAuthProvider } = require("firebase/auth");
+// const { initializeApp } = require("firebase/app");
+// const { getAuth } = require("firebase/auth");
+// const { getFirestore } = require("firebase/firestore");
+// const { GoogleAuthProvider } = require("firebase/auth");
 
-const firebaseConfig = {
-  apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
-  authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
-  projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
-  storageBucket: `${process.env.NEXT_PUBLIC_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
-};
+const admin = require("firebase-admin");
+const serviceAccount = require("../../serviceAccount.json"); // Replace with your service account key
 
-const provider = new GoogleAuthProvider();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://credible-bay-405515.asia-southeast2.firebaseio.com/", // Replace with your database URL
+});
 
-const app = initializeApp(firebaseConfig);
+const db = admin.firestore();
 
-const auth = getAuth(app);
-const db = getFirestore(app);
+// const firebaseConfig = {
+//   apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+//   authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
+//   projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+//   storageBucket: `${process.env.NEXT_PUBLIC_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID}`,
+//   appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
+// };
 
-module.export = { app, auth, db, provider };
+// const provider = new GoogleAuthProvider();
+
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+module.exports = { db };
