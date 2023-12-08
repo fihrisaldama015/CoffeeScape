e1cb972be4fc6615d869245b002c650fd8b5e05c
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-12-09 00:48:47 +0700

    [ADD] feat:calculate rating average

--- src/handler/userRatingHandler.js
+++ src/handler/userRatingHandler.js
@@ -1,13 +1,15 @@
 const arabica = require("../rating");
 const { db, FieldValue } = require("../lib/firebase");
 
-const addRating = (request, h) => {
+const addRating = async (request, h) => {
   const ratingsCollection = db.collection("rating");
+  const recipesCollection = db.collection("recipes");
+
   try {
     arabica.map((rating) => {
       ratingsCollection
         .doc(rating.name)
-        .set(rating)
+        .set({ name: rating.name }, { merge: true })
         .then(() => {
           console.log(`${rating.name} rating added to Firebase!`);
         })
@@ -15,6 +17,32 @@ const addRating = (request, h) => {
           console.error(`Error adding ${rating.name} rating: `, error);
           throw new Error(error);
         });
+      rating.rating.forEach((item) => {
+        ratingsCollection.doc(rating.name).set(
+          {
+            rating: FieldValue.arrayUnion(item),
+          },
+          { merge: true }
+        );
+      });
+
+      const ratingArray = rating.rating;
+      let ratingLength = 0;
+      let ratingSum = 0;
+      ratingArray.forEach((item) => {
+        if (item.rating > 0) {
+          ratingSum += Number(item.rating);
+          ratingLength++;
+        }
+      });
+      const average = ratingSum / ratingLength;
+
+      recipesCollection.doc(rating.name).set(
+        {
+          rating: average,
+        },
+        { merge: true }
+      );
     });
     const response = h.response({
       status: "success",
@@ -31,10 +59,12 @@ const addRating = (request, h) => {
     return response;
   }
 };
+
 const addCoffeeRating = async (request, h) => {
   try {
     const { id } = request.params;
     const { coffeeId, rating } = request.payload;
+
     if (!coffeeId) {
       const response = h.response({
         status: "fail",
@@ -43,6 +73,7 @@ const addCoffeeRating = async (request, h) => {
       response.code(400);
       return response;
     }
+
     const user = await db.collection("users").doc(id).get();
     if (!user.exists) {
       const response = h.response({
@@ -52,8 +83,8 @@ const addCoffeeRating = async (request, h) => {
       response.code(404);
       return response;
     }
-    const recipeExist = await db.collection("recipes").doc(coffeeId).get();
 
+    const recipeExist = await db.collection("recipes").doc(coffeeId).get();
     if (!recipeExist.exists) {
       const response = h.response({
         status: "fail",
@@ -62,6 +93,7 @@ const addCoffeeRating = async (request, h) => {
       response.code(404);
       return response;
     }
+
     if (rating > 10 || rating < 1) {
       const response = h.response({
         status: "fail",
@@ -71,34 +103,52 @@ const addCoffeeRating = async (request, h) => {
       return response;
     }
 
-    const userRef = db.collection("rating").doc(coffeeId);
-
-    const allRating = await userRef.get();
+    const ratingRef = db.collection("rating").doc(coffeeId);
+    const allRating = await ratingRef.get();
 
     const ratingExist = allRating
       .data()
       .rating.find((item) => item.userId === id);
-
     if (ratingExist) {
       const response = h.response({
         status: "fail",
-        message: `User with id: ${id} already give rating to this recipe with id: ${coffeeId}`,
+        message: `This user already give rating to this recipe with id: ${coffeeId}`,
       });
       response.code(400);
       return response;
     }
 
-    await userRef.set(
+    await ratingRef.set(
       {
         rating: FieldValue.arrayUnion({ userId: id, rating }),
       },
       { merge: true }
     );
-    console.log("Rating added successfully");
-    console.log("Updated user data:", { id: user.id, rating: rating });
+
+    const ratingArray = allRating.data().rating;
+    let ratingLength = 0;
+    ratingArray.forEach((rating) => {
+      if (rating.rating > 0) {
+        ratingLength++;
+      }
+    });
+    const ratingSum = ratingArray.reduce((acc, curr) => {
+      return acc + Number(curr.rating);
+    }, 0);
+    const average = (ratingSum + Number(rating)) / (ratingLength + 1);
+
+    const recipesCollection = db.collection("recipes");
+
+    recipesCollection.doc(coffeeId).set(
+      {
+        rating: average,
+      },
+      { merge: true }
+    );
+
     const response = h.response({
       status: "success",
-      message: "add rating successfully",
+      message: "add rating successfully to recipe with id: " + coffeeId,
       data: {
         id: user.id,
         rating: rating,
@@ -149,9 +199,8 @@ const removeRatingCoffee = async (request, h) => {
       return response;
     }
 
-    const userRef = db.collection("rating").doc(coffeeId);
-
-    const allRating = await userRef.get();
+    const ratingRef = db.collection("rating").doc(coffeeId);
+    const allRating = await ratingRef.get();
 
     const ratingExist = allRating
       .data()
@@ -160,7 +209,7 @@ const removeRatingCoffee = async (request, h) => {
     if (!ratingExist) {
       const response = h.response({
         status: "fail",
-        message: `User with id: ${id} not giving rating to recipe with id: ${coffeeId}`,
+        message: `This user is not giving rating to recipe with id: ${coffeeId}`,
       });
       response.code(400);
       return response;
@@ -170,24 +219,46 @@ const removeRatingCoffee = async (request, h) => {
       .data()
       .rating.findIndex((item) => item.userId === id);
 
-    await userRef.update({
+    await ratingRef.update({
       rating: FieldValue.arrayRemove(allRating.data().rating[ratingIndex]),
     });
 
-    console.log("Rating removed successfully");
-    console.log("Updated user data:", { id: user.id });
+    const allRatingAfterDelete = await ratingRef.get();
+    const ratingArray = allRatingAfterDelete.data().rating;
+    let ratingLength = 0;
+    ratingArray.forEach((rating) => {
+      if (rating.rating > 0) {
+        ratingLength++;
+      }
+    });
+    const ratingSum = ratingArray.reduce((acc, curr) => {
+      return acc + Number(curr.rating);
+    }, 0);
+    const average = ratingSum / ratingLength;
+
+    const recipesCollection = db.collection("recipes");
+
+    recipesCollection.doc(coffeeId).set(
+      {
+        rating: average,
+      },
+      { merge: true }
+    );
+
     const response = h.response({
       status: "success",
-      message: "remove favorite successfully",
+      message: "remove rating successfully to recipe with id: " + coffeeId,
       data: {
         id: user.id,
-        favorite: coffeeId,
       },
     });
     response.code(201);
     return response;
   } catch (error) {
-    console.log(error);
+    console.log(
+      "🚀 ~ file: userRatingHandler.js:246 ~ removeRatingCoffee ~ error:",
+      error
+    );
     const response = h.response({
       status: "fail",
       message: "remove favorite failed: " + error,
