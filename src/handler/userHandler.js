1abeb1501aa75fa24b533d5e9a876d9824f7b8ef
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-12-10 00:01:30 +0700

    [ADD] feat:forgot password

--- src/handler/userHandler.js
+++ src/handler/userHandler.js
@@ -1,6 +1,5 @@
-const { db, FieldValue } = require("../lib/firebase");
+const { db } = require("../lib/firebase");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const getAllUsers = async (request, h) => {
   try {
@@ -36,136 +35,28 @@ const getAllUsers = async (request, h) => {
   }
 };
 
-const registerUser = async (request, h) => {
+const getUserById = async (request, h) => {
   try {
-    if (!request.payload) {
+    const { id } = request.params;
+    const user = await db.collection("users").doc(id).get();
+    if (!user.exists) {
       const response = h.response({
         status: "fail",
-        message: "Please add a JSON payload",
-      });
-      response.code(400);
-      return response;
-    }
-
-    const { email, name, password } = request.payload;
-
-    if (!email || !name || !password) {
-      const response = h.response({
-        status: "fail",
-        message:
-          "Please fill all field in JSON, `email`, `name`, and `password`",
-      });
-      response.code(400);
-      return response;
-    }
-
-    const userWithSameEmail = await db
-      .collection("users")
-      .where("email", "==", email)
-      .get();
-
-    if (!userWithSameEmail.empty) {
-      const response = h.response({
-        status: "fail",
-        message: "Email already exist",
-      });
-      response.code(400);
-      return response;
-    }
-
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const data = {
-      email,
-      name,
-      password: hashedPassword,
-    };
-
-    const user = await db.collection("users").add(data);
-    const response = h.response({
-      status: "success",
-      message: "User created successfully",
-      data: {
-        id: user.id,
-        email,
-        name,
-      },
-    });
-    response.code(201);
-    return response;
-  } catch (error) {
-    console.log(error);
-    const response = h.response({
-      status: "fail",
-      message: "User created failed: " + error.message,
-    });
-    response.code(400);
-    return response;
-  }
-};
-
-const loginUser = async (request, h) => {
-  try {
-    if (!request.payload) {
-      const response = h.response({
-        status: "fail",
-        message: "Please add a valid JSON payload",
-      });
-      response.code(400);
-      return response;
-    }
-
-    const { email, password } = request.payload;
-
-    if (!email || !password) {
-      const response = h.response({
-        status: "fail",
-        message: "Please fill all field in JSON, `email` and `password`",
-      });
-      response.code(400);
-      return response;
-    }
-
-    const user = await db.collection("users").where("email", "==", email).get();
-
-    if (user.empty) {
-      const response = h.response({
-        status: "fail",
-        message: "User not found with email: " + email,
+        message: "User not found",
       });
       response.code(404);
       return response;
     }
-
-    const userData = user.docs[0].data();
-    const isMatch = await bcrypt.compare(password, userData.password);
-
-    if (!isMatch) {
-      const response = h.response({
-        status: "fail",
-        message: "Wrong password",
-      });
-      response.code(400);
-      return response;
-    }
-
-    const JWTPayloadData = {
-      id: user.docs[0].id,
-      email: userData.email,
-      name: userData.name,
-      iss: "http://localhost",
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
-      iat: Math.floor(Date.now() / 1000),
-    };
-
-    const token = jwt.sign(JWTPayloadData, process.env.JWT_SECRET_KEY);
-
     const response = h.response({
       status: "success",
-      message: "Login successfully",
+      message: "get User successfully",
       data: {
-        token,
+        id: user.id,
+        email: user.data().email,
+        name: user.data().name,
+        favoriteCoffee: user.data().favoriteCoffee
+          ? user.data().favoriteCoffee
+          : [],
       },
     });
     response.code(200);
@@ -174,12 +65,13 @@ const loginUser = async (request, h) => {
     console.log(error);
     const response = h.response({
       status: "fail",
-      message: "Login failed: " + error.message,
+      message: "get User failed: " + error,
     });
     response.code(400);
     return response;
   }
 };
+
 const updateUser = async (request, h) => {
   try {
     const { id } = request.params;
@@ -244,10 +136,11 @@ const updateUser = async (request, h) => {
     return response;
   }
 };
-const getUserById = async (request, h) => {
+
+const deleteUser = async (request, h) => {
   try {
     const { id } = request.params;
-    const user = await db.collection("users").doc(id).get();
+    let user = await db.collection("users").doc(id).get();
     if (!user.exists) {
       const response = h.response({
         status: "fail",
@@ -256,160 +149,66 @@ const getUserById = async (request, h) => {
       response.code(404);
       return response;
     }
+    await db.collection("users").doc(id).delete();
     const response = h.response({
       status: "success",
-      message: "get User successfully",
-      data: {
-        id: user.id,
-        email: user.data().email,
-        name: user.data().name,
-        favoriteCoffee: user.data().favoriteCoffee
-          ? user.data().favoriteCoffee
-          : [],
-      },
+      message: "User deleted successfully",
     });
     response.code(200);
     return response;
   } catch (error) {
-    console.log(error);
+    console.log("🚀 ~ file: userHandler.js:161 ~ deleteUser ~ error:", error);
     const response = h.response({
       status: "fail",
-      message: "get User failed: " + error,
+      message: "User deleted failed: " + error.message,
     });
     response.code(400);
     return response;
   }
 };
 
-const addFavoriteCoffee = async (request, h) => {
+const forgotPassword = async (request, h) => {
   try {
-    const { id } = request.params;
-    const { coffeeId } = request.payload;
-    if (!coffeeId) {
+    const { email, password } = request.payload;
+    if (!email) {
       const response = h.response({
         status: "fail",
-        message: "Please fill all field, `coffeeId`",
+        message: "Please fill `email` field",
       });
       response.code(400);
       return response;
     }
-    const user = await db.collection("users").doc(id).get();
-    if (!user.exists) {
+    const user = await db.collection("users").where("email", "==", email).get();
+    if (user.empty) {
       const response = h.response({
         status: "fail",
-        message: "User not found",
+        message: "User not found with email: " + email,
       });
       response.code(404);
       return response;
     }
-    const recipeExist = await db.collection("recipes").doc(coffeeId).get();
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const data = {
+      password: hashedPassword,
+    };
+    let userRef = db.collection("users").doc(user.docs[0].id);
+    await userRef.set(data, { merge: true });
 
-    if (!recipeExist.exists) {
-      const response = h.response({
-        status: "fail",
-        message: "Recipe with id:`" + coffeeId + "` not found",
-      });
-      response.code(404);
-      return response;
-    }
-
-    const userRef = db.collection("users").doc(id);
-    await userRef.set(
-      {
-        favoriteCoffee: FieldValue.arrayUnion(coffeeId),
-      },
-      { merge: true }
-    );
     const response = h.response({
       status: "success",
-      message: "add favorite coffee successfully",
-      data: {
-        id: user.id,
-        email: user.data().email,
-        name: user.data().name,
-        favoriteCoffee: user.data().favoriteCoffee,
-      },
+      message: "User password updated successfully",
     });
-    response.code(201);
+    response.code(200);
     return response;
   } catch (error) {
-    console.log(error);
-    const response = h.response({
-      status: "fail",
-      message: "add favorite coffee failed: " + error,
-    });
-    response.code(400);
-    return response;
-  }
-};
-
-const removeFavoriteCoffee = async (request, h) => {
-  try {
-    const { id } = request.params;
-    const { coffeeId } = request.payload;
-    if (!coffeeId) {
-      const response = h.response({
-        status: "fail",
-        message: "Please fill all field, `coffeeId`",
-      });
-      response.code(400);
-      return response;
-    }
-    const user = await db.collection("users").doc(id).get();
-    if (!user.exists) {
-      const response = h.response({
-        status: "fail",
-        message: "User not found",
-      });
-      response.code(404);
-      return response;
-    }
-    const recipeExist = await db.collection("recipes").doc(coffeeId).get();
-
-    if (!recipeExist.exists) {
-      const response = h.response({
-        status: "fail",
-        message: "Recipe with id:`" + coffeeId + "` not found",
-      });
-      response.code(404);
-      return response;
-    }
-
-    const favoriteCoffeeExist = user.data().favoriteCoffee.includes(coffeeId);
-
-    if (!favoriteCoffeeExist) {
-      const response = h.response({
-        status: "fail",
-        message: "Recipe with id:`" + coffeeId + "` not found in favorite",
-      });
-      response.code(404);
-      return response;
-    }
-
-    const userRef = db.collection("users").doc(id);
-    await userRef.set(
-      {
-        favoriteCoffee: FieldValue.arrayRemove(coffeeId),
-      },
-      { merge: true }
+    console.log(
+      "🚀 ~ file: userHandler.js:197 ~ forgotPassword ~ error:",
+      error
     );
     const response = h.response({
-      status: "success",
-      message: "remove favorite coffee successfully",
-      data: {
-        id: user.id,
-        email: user.data().email,
-        name: user.data().name,
-        favoriteCoffee: user.data().favoriteCoffee,
-      },
-    });
-    response.code(201);
-    return response;
-  } catch (error) {
-    console.log(error);
-    const response = h.response({
       status: "fail",
-      message: "remove favorite coffee failed: " + error,
+      message: "User found failed: " + error.message,
     });
     response.code(400);
     return response;
@@ -418,10 +217,8 @@ const removeFavoriteCoffee = async (request, h) => {
 
 module.exports = {
   getAllUsers,
-  registerUser,
-  loginUser,
-  updateUser,
   getUserById,
-  addFavoriteCoffee,
-  removeFavoriteCoffee,
+  updateUser,
+  deleteUser,
+  forgotPassword,
 };
